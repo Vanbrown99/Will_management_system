b422012121857ad15a48d8eec143da9ebaac1da7
Vanbrown99 <vanbrown920@gmail.com>
2026-06-07 17:40:10 +0200

    Update hardhat.config.js

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,1 +1,30 @@
- 
+ import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config({ path: '../backend/.env' });
+
+module.exports = {
+  solidity: {
+    version: '0.8.19',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs:    200,
+      },
+    },
+  },
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+    },
+    mumbai: {
+      url:      process.env.RPC_URL || '',
+      accounts: process.env.DEPLOYER_PRIVATE_KEY
+        ? [process.env.DEPLOYER_PRIVATE_KEY]
+        : [],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || '',
+    },
+  },
+};
